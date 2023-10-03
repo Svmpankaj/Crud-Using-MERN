8cd5fcf73e2ac0de25665e1ded1ecc26be01c91c
Svmpankaj <svmpankaj@gmail.com>
2023-10-03 23:46:43 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
