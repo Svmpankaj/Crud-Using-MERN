550a19a494223f4fcda90d7b79d438539281fd52
Svmpankaj <svmpankaj@gmail.com>
2023-10-05 22:55:20 +0530

    create input form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,19 @@ const Home = () => {
   return (
     <div>
       <h1 className='text-center'>Crud App</h1>
-
+        <div>
+          <form action="">
+            <div>
+              <label htmlFor="name">Name</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="">Email</label>
+              <input type="text" />
+            </div>
+            <button>Add</button>
+          </form>
+        </div>
     </div>
   )
 }
