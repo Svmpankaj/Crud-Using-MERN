90bf6c3c8dde196d100e47b07a6bb1d76568e8a1
Svmpankaj <svmpankaj@gmail.com>
2023-10-06 23:54:47 +0530

    form align at center

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='min-h-screen bg-[#004b43]'>
       <h1 className='text-center'>Crud App</h1>
-        <div>
+        <div className='bg-[#e5e4e4] max-w-fit m-auto p-10'>
           <form action="">
-            <div>
+            <div className='flex flex-col'>
               <label htmlFor="name">Name</label>
               <input type="text" />
             </div>
-            <div>
+            <div className='flex flex-col'>
               <label htmlFor="">Email</label>
               <input type="text" />
             </div>
-            <button>Add</button>
+            <button className='w-full b-[#014d64] text-white mt-3'>Add</button>
           </form>
         </div>
     </div>
