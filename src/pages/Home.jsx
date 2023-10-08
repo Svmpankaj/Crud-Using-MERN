772caf1efe5b4548ad8c160f73c69bada263a183
Svmpankaj <svmpankaj@gmail.com>
2023-10-08 23:55:43 +0530

    change input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,16 @@ const Home = () => {
     email: ""
   })
 
+  const handleChange = (e) => {
+    setInputs(
+      {
+        ...inputs,
+        [e.target.name]: e.target.value,
+        
+      }
+    )
+  }
+
   return (
     <div className='min-h-screen bg-[#004b43]'>
       <h1 className='text-center'>Crud App</h1>
@@ -13,11 +23,11 @@ const Home = () => {
           <form action="">
             <div className='flex flex-col'>
               <label htmlFor="name">Name</label>
-              <input type="text" name='name' />
+              <input type="text" name='name' value={inputs.name} onChange={handleChange}/>
             </div>
             <div className='flex flex-col'>
               <label htmlFor="">Email</label>
-              <input type="text" />
+              <input type="email" name='email' value={inputs.email} onChange={handleChange} />
             </div>
             <button className='w-full b-[#014d64] text-white mt-3'>Add</button>
           </form>
