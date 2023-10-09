db05022b6de4cb75e79a00cc47fa965134f328fe
Svmpankaj <svmpankaj@gmail.com>
2023-10-09 23:35:41 +0530

    fixed randering page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,24 +3,32 @@ import React, { useState } from 'react'
 const Home = () => {
   const [inputs, setInputs] = useState({
     name: "",
-    email: ""
-  })
+    email: "",
+  });
+
+  const [tableData, setTableData] = useState([])
 
   const handleChange = (e) => {
     setInputs(
       {
         ...inputs,
-        [e.target.name]: e.target.value,
-        
-      }
-    )
-  }
+        [e.target.name]: e.target.value, 
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("inputs", inputs);
+    setTableData([...tableData, inputs])
+  };
+
+  console.log("tableData", tableData)
 
   return (
     <div className='min-h-screen bg-[#004b43]'>
       <h1 className='text-center'>Crud App</h1>
         <div className='bg-[#e5e4e4] max-w-fit m-auto p-10'>
-          <form action="">
+          <form action="" onSubmit={handleSubmit}>
             <div className='flex flex-col'>
               <label htmlFor="name">Name</label>
               <input type="text" name='name' value={inputs.name} onChange={handleChange}/>
@@ -29,7 +37,7 @@ const Home = () => {
               <label htmlFor="">Email</label>
               <input type="email" name='email' value={inputs.email} onChange={handleChange} />
             </div>
-            <button className='w-full b-[#014d64] text-white mt-3'>Add</button>
+            <button type='submit' className='w-full b-[#014d64] text-white mt-3'>Add</button>
           </form>
         </div>
     </div>
